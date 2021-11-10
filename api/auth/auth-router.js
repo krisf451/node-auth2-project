dc56596b2516e68bb5045d1824f0dafa062de4f4
Kristian Fulkerson <kristianf451@gmail.com>
2021-11-10 08:59:01 -0800

    not sure

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,21 +2,9 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const tokenBuilder = require("./token-builder");
 const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
-const { JWT_SECRET } = require("../secrets"); // use this secret!
 const Users = require("../users/users-model");
 
 router.post("/register", validateRoleName, (req, res, next) => {
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
   let { username, password } = req.body;
   const { role_name } = req;
   const hash = bcrypt.hashSync(password, 6);
@@ -28,25 +16,6 @@ router.post("/register", validateRoleName, (req, res, next) => {
 });
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
   const { username, password } = req.body;
   Users.findBy({ username })
     .then(([user]) => {
